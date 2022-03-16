e0592a9fa6009735d26c68ff3cc7ccbc635f75f5
OlgaDorokhova <flighter@mail.ru>
2022-03-16 16:25:37 +0300

    Initial commit. API для экспорта данных из Clickhouse. Должы быть реализованы фильтры/постраничная загрузка.

---
+++ FirstModule/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,12 @@
+import org.jetbrains.annotations.TestOnly
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+
+				@Test
+				fun `first test ` () {
+								assertEquals(3, 2 + 1)
+				}
+
+}
